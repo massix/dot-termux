edf1a4d8662dec145b43191ba00dbd67595ab8c2
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2023-12-29 10:52:02 +0100

    nvim: fix lualine display

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,3 +1,15 @@
+function fg(name)
+  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
+  local fg = hl and hl.fg or hl.foreground
+  return fg and { fg = string.format("#%06x", fg) }
+end
+
+local git_icons = {
+  added = " ",
+  modified = " ",
+  removed = " ",
+}
+
 return {
   -- statusline
   {
@@ -40,13 +52,25 @@ return {
             },
           },
           lualine_x = {
-            -- stylua: ignore
-            -- stylua: ignore
-            -- stylua: ignore
-            -- stylua: ignore
             { require("lazy.status").updates, cond = require("lazy.status").has_updates, },
             {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              color = fg("Statement"),
+            },
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+              color = fg("Constant"),
+            },
+            {
               "diff",
+              symbols = {
+                added = git_icons.added,
+                modified = git_icons.modified,
+                removed = git_icons.removed,
+              },
             },
           },
           lualine_y = {
