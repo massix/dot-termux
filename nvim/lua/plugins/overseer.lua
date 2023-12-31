3a0c208993ce6652e3994033ab9b3a0a2f82f8d5
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2023-12-31 14:02:02 +0100

    nvim: install overseer

---
+++ nvim/lua/plugins/overseer.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "stevearc/overseer.nvim",
+    opts = {},
+    lazy = false,
+    event = "VeryLazy",
+    init = function()
+      local wk = require("which-key")
+      wk.register({
+        ["<C-c>o"] = { name = "+overseer" },
+      })
+    end,
+    keys = {
+      { "<C-c>ot", [[<cmd>OverseerToggle<cr>]], desc = "Toggle Overseer" },
+      { "<C-c>or", [[<cmd>OverseerRun<cr>]], desc = "Run Overseer" }
+    },
+  },
+}
+
