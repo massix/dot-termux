3c55c088b5d92dd1bab2fd7887b918ec3fa65933
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-01-09 22:26:06 +0100

    nvim: better `nomodoro` configuration

--- nvim/lua/plugins/pomodoro.lua
+++ nvim/lua/plugins/pomodoro.lua
@@ -2,7 +2,12 @@ return {
   {
     "dbinagi/nomodoro",
     lazy = true,
-    cmd = { "NomoWork", "NomoTimer", "NomoBreak" },
+    cmd = {
+      "NomoWork",
+      "NomoTimer",
+      "NomoBreak",
+      "NomoMenu"
+    },
     init = function()
       local wk = require("which-key")
       wk.register({
@@ -11,12 +16,28 @@ return {
     end,
     opts = {
       work_time = 40,
-      break_time = 10,
+      short_break_time = 10,
+      long_break_time = 20,
+      break_cycle = 4,
+      menu_available = true,
+      texts = {
+        on_break_complete = "TIME IS UP!",
+        on_work_complete = "TIME IS UP!",
+        status_icon = " ",
+        timer_format = '!%0M:%0S'
+      },
+      on_work_complete = function()
+        vim.notify("Take a break")
+      end,
+      on_break_complete = function()
+        vim.notify("Go back to work")
+      end,
     },
     keys = {
-      { "<leader>pp", [[<cmd>NomoWork<cr>]], desc = "Pomodoro Work" },
+      { "<leader>p<cr>", [[<cmd>NomoWork<cr>]], desc = "Pomodoro Work" },
       { "<leader>ps", [[<cmd>NomoStop<cr>]], desc = "Pomodoro Stop" },
       { "<leader>pb", [[<cmd>NomoBreak<cr>]], desc = "Pomodoro Break" },
+      { "<leader>pp", [[<cmd>NomoMenu<cr>]], desc = "Pomodoro Menu" },
     },
   },
 }
