6e35817d70daa0e5adc77ce61a7c973ba892dbf8
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-02-01 06:13:48 +0100

    fix(nvim): reactive and telescope integration bug

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -278,5 +278,19 @@ return {
     opts = {
       load = { "catppuccin-mocha-cursor", "catppuccin-mocha-cursorline" },
     },
+    config = function(_, opts)
+      require("reactive").setup(opts)
+
+      -- issue: https://github.com/nvim-telescope/telescope.nvim/issues/2027#issuecomment-1561836585
+      vim.api.nvim_create_autocmd("WinLeave", {
+        callback = function()
+          if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+          end
+        end,
+      })
+    end,
+
+
   },
 }
