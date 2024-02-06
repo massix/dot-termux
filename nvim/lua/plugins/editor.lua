5af9c6f9d26f33a614e657e161bb2b1c4cf77dc9
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-02-06 23:36:52 +0100

    feat(nvim): add neowords plugin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -754,4 +754,25 @@ return {
       leader_key = ";",
     },
   },
+
+  -- Better e, w and friends
+  {
+    "backdround/neowords.nvim",
+    event = "VeryLazy",
+    config = function()
+      local neowords = require("neowords")
+      local p = neowords.pattern_presets
+      local subword_hops = neowords.get_word_hops(
+        p.snake_case,
+        p.camel_case,
+        p.upper_case,
+        p.number,
+        p.hex_color)
+
+      vim.keymap.set({ "n", "x", "o" }, "w", subword_hops.forward_start)
+      vim.keymap.set({ "n", "x", "o" }, "e", subword_hops.forward_end)
+      vim.keymap.set({ "n", "x", "o" }, "b", subword_hops.backward_start)
+      vim.keymap.set({ "n", "x", "o" }, "ge", subword_hops.backward_end)
+    end,
+  }
 }
