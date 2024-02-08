46554601246de72db719183dbca0817958f9045d
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-02-08 19:21:52 +0100

    feat(coding): add coding packages

---
+++ coding/install_coding.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+# Import common functions and variables
+set current_dir (realpath (dirname (status -f)))
+source "$current_dir/../scripts/library.fish"
+
+info "Installing coding goodies"
+
+check_install gcc clang
+check_install pkg-config pkg-config
+check_install node nodejs
+
