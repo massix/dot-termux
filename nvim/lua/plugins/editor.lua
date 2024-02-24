9306c36faac9502a3638e0a1886a8fa9b115fe66
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-02-24 18:04:13 +0100

    feat(nvim): add smart tab plugin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -752,4 +752,11 @@ return {
     end,
     config = false,
   },
+
+  -- Smart tab, similar to Helix's
+  {
+    "boltlessengineer/smart-tab.nvim",
+    opts = {},
+    event = { "BufEnter", "BufWinEnter" },
+  },
 }
