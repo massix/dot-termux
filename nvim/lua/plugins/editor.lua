51973abdc7a567134b25d020ea6e8bf8e25cca26
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-02-26 15:30:15 +0100

    feat(nvim): include mini.align

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -772,4 +772,14 @@ return {
     opts = {},
     ft = { "qf" },
   },
+
+  -- Align easily
+  {
+    "echasnovski/mini.align",
+    opts = {},
+    keys = {
+      { "ga", mode = { "n", "v" }, desc = "Align" },
+      { "gA", mode = { "n", "v" }, desc = "Align with preview" },
+    },
+  },
 }
