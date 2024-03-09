c4610c32ecbe77b84cbdd1836d09bfe6c57bc4d8
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-03-09 14:43:56 +0100

    feat(nvim): change foldsigns

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -313,7 +313,14 @@ return {
   -- Foldsign
   {
     "yaocccc/nvim-foldsign",
-    opts = {},
+    opts = {
+      offset = -2,
+      foldsigns = {
+        open = "󰅀", -- mark the beginning of a fold
+        close = "󰅂", -- show a closed fold
+        seps = { "", "" }, -- open fold middle marker
+      },
+    },
     event = { "BufEnter", "BufWinEnter" },
   },
 }
