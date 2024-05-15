3b7e5882ef3b4ccb7466247f199002cc1c93dbf3
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-05-15 19:10:46 +0200

    feat(termux): change font

--- termux/install_termux.fish
+++ termux/install_termux.fish
@@ -8,7 +8,7 @@ set termux_dir "$HOME/.termux"
 set -l cache_dir "$HOME/.cache/installer"
 mkdir -p {$cache_dir}/font
 
-set -l font_url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/0xProto.zip"
+set -l font_url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IBMPlexMono.zip"
 
 check_install curl curl
 
@@ -26,7 +26,7 @@ info "Unpacking font"
 unzip -qq -o -x "$cache_dir/font.zip" -d "$cache_dir/font/"
 
 info "Copying patched font"
-cp {$cache_dir}/font/0xProtoNerdFont-Regular.ttf {$termux_dir}/font.ttf
+cp {$cache_dir}/font/BlexMonoNerdFont-Regular.ttf {$termux_dir}/font.ttf
 
 info "Cleaning cache folder"
 rm -rf {$cache_dir}/font/*
