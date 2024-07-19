604f8e561971c530db9ca0940f6632e4136fca4d
Massimo Gengarelli <massimo.gengarelli@proton.me>
2024-07-19 18:25:47 +0200

    style: change telescope default theme

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -12,7 +12,7 @@ return {
     opts = function()
       -- change default theme
       return {
-        defaults = vim.tbl_extend("force", require("telescope.themes").get_ivy(), {
+        defaults = vim.tbl_extend("force", require("telescope.themes").get_dropdown(), {
           prompt_prefix = " ",
           selection_caret = " ",
         }),
